311d965d0622adcf429854f1954b2df1eef30b9f
mondara1234 <40348446+mondara1234@users.noreply.github.com>
2018-07-28 22:56:39 +0700

    fg

--- index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { nameProject as AppGugg } from './app.json';
-import App from './App';
-
-const ProjectEnd = () => <App />;
-
-export default ProjectEnd;
-
-AppRegistry.registerComponent(AppGugg, () => ProjectEnd);
