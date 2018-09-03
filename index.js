f6e30c1c3edd361230e834ea6790ba099aec816b
mondara1234 <40348446+mondara1234@users.noreply.github.com>
2018-09-03 14:34:50 +0700

    edit

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { YellowBox, AppRegistry } from 'react-native'
 import { nameProject as AppGugg } from './app.json';
-<<<<<<< HEAD
 import App from './src/app';
-=======
-import App from './src/features/User/components/imagePicker';
-import ImageSuccessScreen from "./ImageSuccessScreen";
->>>>>>> f1573f805513bc8dd4c9d0249a170845b766ebca
 
 const ProjectEnd = () => <App />;
 
@@ -14,4 +9,4 @@ YellowBox.ignoreWarnings([
     'Remote debugger',
 ]);
 
-AppRegistry.registerComponent(AppGugg, () => ImageSuccessScreen);
+AppRegistry.registerComponent(AppGugg, () => ProjectEnd);
